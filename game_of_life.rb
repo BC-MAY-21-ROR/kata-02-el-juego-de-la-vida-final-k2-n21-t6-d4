5e9f2aaf39b1d14ed36465f120a7ff6c4376b8ad
javs1 <59986245+javs1@users.noreply.github.com>
2021-11-30 16:05:27 -0600

    Method generate created
    This method fill init with live and death cells random , and future
    ready just with death cells
    
    Co-authored-by: BrandonVG <bvadillo0@ucol.mx>
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>
    Co-authored-by: pastorp3  <pastorpedraza3@gmail.com>

--- game_of_life.rb
+++ game_of_life.rb
@@ -8,4 +8,17 @@ class GameOfLive
     @init = Array.new(@height) { Array.new(@width) }
     @future = Array.new(@height) { Array.new(@width) }
   end
+
+  def generate
+    (0..(@height - 1)).each do |h|
+      (0..(@width - 1)).each do |w|
+        @future[h][w] = 0
+        @init[h][w] = if [0, 1, 2].include?(rand(4))
+                          0
+                        else
+                          1
+                        end
+      end
+    end
+  end
 end
